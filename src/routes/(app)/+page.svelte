9f0bf286e14d6e896e3a2283530d1b6d1a604b39
Shigero Toura <ilarumbgim1@educacion.navarra.es>
2023-03-19 19:52:11 +0100

    fix: various fixes

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,19 +1,15 @@
 <script>
-	import {
-		Tile,
-		Link,
-		Button
-	} from 'carbon-components-svelte';
-
-	const spoofImage =
-		'https://st2.depositphotos.com/1028880/11190/i/450/depositphotos_111903254-stock-photo-turkish-national-flag-waving-in.jpg';
+	import { Tile, Link, Button } from 'carbon-components-svelte';
+	import { user } from '../../stores/user';
 </script>
 
 <h1 class="text-4xl">Welcome to <span class="font-bold">ANOMIA</span></h1>
 <p>Anomia is the privacy and SBC hub open for discussion, news and many other events!</p>
-<p class="text-sm">
-	To get started <Link href="/register">make an account</Link> or <Link href="/login">login</Link>
-</p>
+{#if $user.id == 0}
+	<p class="text-sm">
+		To get started <Link href="/register">make an account</Link> or <Link href="/login">login</Link>
+	</p>
+{/if}
 
 <h2 class="py-4 text-2xl font-bold">Recent News</h2>
 <div class="grid grid-cols-1 md:grid-cols-6 gap-4">
@@ -24,8 +20,8 @@
 			alt=""
 			srcset=""
 		/>
-		<p class="text-2xl hover:underline pt-3 ">Edrogan fucking dies</p>
-		<p class="pb-2">The President of turkey fucking died the other day due to brain injury, lmao</p>
+		<p class="text-2xl hover:underline pt-3 ">PLACEHOLDER</p>
+		<p class="pb-2">This is a placeholder, the actual news will come soon! :-)</p>
 		<Button kind="tertiary" class="">Read More</Button>
 	</Tile>
 </div>
@@ -39,8 +35,8 @@
 			alt=""
 			srcset=""
 		/>
-		<p class="text-2xl hover:underline pt-3 ">Polytoria Egg Hunt!</p>
-		<p class="pb-2">Polytoria's Egg Hunt is in the wild!</p>
-		<Button kind="danger" class="border border-white">Participate!</Button>
+		<p class="text-2xl hover:underline pt-3 ">PLACEHOLDER</p>
+		<p class="pb-2">This is a placeholder, the actual news will come soon! :-)</p>
+		<Button kind="danger" class="border border-white">Read More</Button>
 	</Tile>
 </div>
